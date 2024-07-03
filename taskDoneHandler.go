70604fafe7dc3abc2140bca10d6e052670cfd670
Konstantin Fedorov <constantineborsa@gmail.com>
2024-07-03 22:17:28 +0300

    second

---
+++ taskDoneHandler.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func taskAsDoneHandler(w http.ResponseWriter, r *http.Request) {
+	// Проверяем метод запроса
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Получаем значение параметра id из запроса
+	id := r.URL.Query().Get("id")
+
+	// Проверяем, что id не пустой
+	if id == "" {
+		erresponse := ErrorResponse{Error: "Не указан идентификатор"}
+		sendErrorResponse(w, http.StatusBadRequest, erresponse)
+		return
+	}
+
+	var err error
+
+	// Получаем задачу из базы данных для дальнейших операций
+	var task Task
+	query := `SELECT id, date, title, comment, repeat FROM scheduler WHERE id = ?`
+	if err := DB.Get(&task, query, id); err != nil {
+		log.Printf("Failed to retrieve task from database: %v\n", err)
+		response := ErrorResponse{Error: "Задача не найдена"}
+		sendErrorResponse(w, http.StatusNotFound, response)
+		return
+	}
+	now := time.Now()
+	nowDate := now.Format("20060102")
+	taskDate := task.Date
+
+	// Если задача периодическая, вычисляем следующую дату выполнения
+	if task.Repeat != "" {
+
+		nextDate, err := NextDate(now, taskDate, task.Repeat)
+		if err != nil {
+			response := ErrorResponse{Error: fmt.Sprintf("Failed to calculate next date: %s", err.Error())}
+			sendErrorResponse(w, http.StatusInternalServerError, response)
+			return
+		}
+
+		// Если now и taskDate равны, добавляем интервал days к текущей дате
+		if nowDate == taskDate {
+			repeatParts := strings.Fields(task.Repeat)
+			if len(repeatParts) == 2 {
+				days, err := strconv.Atoi(repeatParts[1])
+				if err == nil {
+					nextDate = now.AddDate(0, 0, days).Format("20060102")
+				}
+			}
+		}
+
+		// Обновляем дату задачи
+		updateSQL := `UPDATE scheduler SET date = ? WHERE id = ?`
+		if _, err = DB.Exec(updateSQL, nextDate, id); err != nil {
+			log.Printf("Failed to update task date in database: %v\n", err)
+			response := ErrorResponse{Error: "Ошибка при обновлении даты задачи"}
+			sendErrorResponse(w, http.StatusInternalServerError, response)
+			return
+		}
+	} else {
+		// Если значение repeat пустое, удаляем задачу из базы данных
+		deleteSQL := `DELETE FROM scheduler WHERE id = ?`
+		if _, err = DB.Exec(deleteSQL, id); err != nil {
+			log.Printf("Failed to delete task from database: %v\n", err)
+			response := ErrorResponse{Error: "Ошибка при удалении задачи"}
+			sendErrorResponse(w, http.StatusInternalServerError, response)
+			return
+		}
+	}
+
+	// Успешный ответ
+	sendResponse(w, http.StatusOK, map[string]interface{}{})
+}
---
+++ updateTaskHandler.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func updateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var task Task
+	err := json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+
+	// Проверяем обязательное поле ID
+	if task.ID == "" {
+		response := ErrorResponse{Error: "Не указан идентификатор задачи (ID)"}
+		sendErrorResponse(w, http.StatusBadRequest, response)
+		return
+	}
+
+	// Проверяем обязательное поле Title
+	if task.Title == "" {
+		response := ErrorResponse{Error: "Title field is required"}
+		sendErrorResponse(w, http.StatusBadRequest, response)
+		return
+	}
+
+	// Проверяем формат даты и преобразуем в формат 20060102
+	date := task.Date
+	if date == "" {
+		date = time.Now().Format("20060102")
+	}
+
+	parsedDate, err := time.Parse("20060102", date)
+	if err != nil {
+		response := ErrorResponse{Error: "Invalid date format, should be YYYYMMDD"}
+		sendErrorResponse(w, http.StatusBadRequest, response)
+		return
+	}
+
+	// Если дата задачи меньше текущей, вычисляем следующую дату выполнения
+	if parsedDate.Before(time.Now()) {
+		if task.Repeat != "" {
+			nextDate, err := NextDate(time.Now(), date, task.Repeat)
+			if err != nil {
+				response := ErrorResponse{Error: fmt.Sprintf("Failed to calculate next date: %s", err.Error())}
+				sendErrorResponse(w, http.StatusBadRequest, response)
+				return
+			}
+			task.Date = nextDate
+		} else {
+			task.Date = time.Now().Format("20060102")
+		}
+	}
+
+	// Выполняем SQL-запрос для обновления задачи
+	updateSQL := `UPDATE scheduler SET date=?, title=?, comment=?, repeat=? WHERE id=?`
+	result, err := DB.Exec(updateSQL, task.Date, task.Title, task.Comment, task.Repeat, task.ID)
+	if err != nil {
+		log.Printf("Failed to update task in database: %v\n", err)
+		response := ErrorResponse{Error: "Failed to update task in database"}
+		sendErrorResponse(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		log.Printf("Failed to get rows affected: %v\n", err)
+		response := ErrorResponse{Error: "Failed to get rows affected"}
+		sendErrorResponse(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	// Проверяем, была ли обновлена хотя бы одна запись
+	if rowsAffected == 0 {
+		response := ErrorResponse{Error: "Задача не найдена"}
+		sendErrorResponse(w, http.StatusNotFound, response)
+		return
+	}
+
+	// Возвращаем успешный ответ
+	sendResponse(w, http.StatusOK, map[string]string{})
+}
